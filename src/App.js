346bddd46083ccf0128d80aa46ffb924ede1e8b1
Isaac Harari <hararimasri@gmail.com>
2021-04-25 02:34:59 -0500

    updating app with shop page; coll preview cpnt; coll item cpnt

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
-import HomePage from "./pages/homepage/homepage.component";
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+// const HatsPage = () => (
+//   <div>
+//     <h1>HATS PAGE</h1>
+//   </div>
+// );
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact={true} path="/" component={HomePage} />
-        <Route exact={true} path="/hats" component={HatsPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
