36cc7cd15c3ddf958b5c737e2d417a8e434647b5
Isaac Harari <hararimasri@gmail.com>
2021-05-27 22:29:34 -0500

    added persist (localstorage) to cart reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,10 +3,23 @@
 // this action is an object that has a type (string) that tells the specific action and a payload (any)
 
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+
+    // Name of the reducers we want to persist
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 })
+
+export default persistReducer( persistConfig, rootReducer )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,14 @@
 // A store holds the whole state tree of your application. The only way to change the state inside it is to dispatch an action on it.
 // A store is not a class. It's just an object with a few methods on it. To create it, pass your root reducing function to createStore.
 
-import { createStore, applyMiddleware } from 'redux';
-import { logger } from 'redux-logger';
-import rootReducer from './root-reducer';
+import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
+import { logger } from "redux-logger";
+
+import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
