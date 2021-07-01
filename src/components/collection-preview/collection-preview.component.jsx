1b88af8d329427a8afa3b32a387475bca0160eba
Isaac Harari <hararimasri@gmail.com>
2021-07-01 00:53:16 -0500

    redirect to a category collection when clicking its name in the general shop page

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { withRouter } from 'react-router-dom';
+
 import CollectionItem from "../collection-item/collection-item.component.jsx";
 
 // import "./collection-preview.styles.scss";
@@ -8,17 +10,21 @@ import {
   PreviewContainer,
 } from "./collection-preview.styles";
 
-const CollectionPreview = ({ title, items }) => (
-  <CollectionPreviewContainer>
-    <TitleHeader>{title.toUpperCase()}</TitleHeader>
-    <PreviewContainer>
-      {items
-        .filter((item, idx) => idx < 4)
-        .map((item) => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-    </PreviewContainer>
-  </CollectionPreviewContainer>
-);
+const CollectionPreview = ({ title, items, history, match, routeName }) => {
+  return (
+    <CollectionPreviewContainer>
+      <TitleHeader onClick={() => history.push(`${match.path}/${routeName}`)}>
+        {title.toUpperCase()}
+      </TitleHeader>
+      <PreviewContainer>
+        {items
+          .filter((item, idx) => idx < 4)
+          .map((item) => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </PreviewContainer>
+    </CollectionPreviewContainer>
+  );
+};
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
